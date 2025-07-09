0f18283531272b2c84598f7758a20377bab3f00b
Wafiddin <wafiddinnaufal@gmail.com>
2025-07-09 21:05:28 +0700

    chore: init about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,69 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h1>
+        About Pokémon Awesome
+      </h1>
+      <p>
+        Pokémon Awesome is basically just another random Pokémon-related web-application that appeared in the wild.
+      </p>
+      <p>
+        The creator of this web application developed it simply for the sake of having fun, while also learning various aspects of software engineering and UI/UX design.
+      </p>
+      <p>
+        <h1>Contact:</h1>
+        <button className="btn btn-outline btn-primary">LinkedIn</button>
+        <button className="btn btn-outline btn-primary">X</button>
+      </p>
+      <h1>
+        Content & Copyright
+      </h1>
+      <p>
+        This unofficial web application offers certain information about Pokémon series or games. However, arts, visuals, and names featured herein are the properties of Nitendo, Game Freak, & The Pokémon Company.
+      </p>
+      <p>
+        Please note that this web application is not official and is not linked to the company mentioned above. Some images used in this web application are copyrighted and belong to Nintendo, Game Freak, or The Pokémon Company. They are used in accordance with the laws of Fair Use. No copyright infringement intended.
+      </p>
+      <p>The main data sources for the content of this web application are:
+      </p>
+      <li>
+        PokeAPI (https://pokeapi.co/)
+      </li>
+      <li>
+        Pokémon TCG API (https://pokemontcg.io/)
+      </li>
+      <p>
+        Special thanks to Simon Goellner for his awesome open-source Pokemon cards animation on https://poke-holo.simey.me/.
+      </p>
+
+      <h1>
+        Privacy Policy
+      </h1>
+      <p>
+        Here's a straightforward summary of how we handle your information:
+      </p>
+      <li>
+        Information Collection: No personal information is required to access this website or view any regular pages.
+      </li>
+      <li>
+        Third-Party Links: Our website may contain links to third-party websites. Please note that we are not responsible for their privacy practices. We encourage you to review their privacy policies.
+      </li>
+      <li>
+        Limitation of Liability: We do not guarantee uninterrupted, secure, or error-free access to the website, and we make no warranties regarding the accuracy of our content. We shall not be liable for any interruptions, delays, errors in the operation of the website, or any damages arising from its use.
+      </li>
+      <li>
+        Consent: By using our website, you consent to our Privacy Policy and agree to its terms.
+      </li>
+      <p>
+        — Last updated at 06 July 2025.
+      </p>
+      <p>
+        
+      </p>
+    </div>
+
+  );
 }
 
-export default About
+export default About;
